1d9c9162cbca18d3cb6bc0be4db80d1e412f46a4
Mohammed Abdul Aziz Syed <smdaziz@gmail.com>
2022-12-28 10:17:20 -0600

    adding google sso

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,27 @@
 import { Container } from "react-bootstrap";
 import { AuthProvider } from "../contexts/AuthContext";
 import Signup from "./Signup";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+import Reset from "./Reset";
+import Dashboard from "./Dashboard";
 
 function App() {
   return (
     <Container className="d-flex align-items-center justify-content-center" style={{ minHeight: "100vh" }}>
       <div className="w-100" style={{ maxWidth: '400px' }}>
-        <Router>
+        <Router basename="rummy-score-tracker">
           <AuthProvider>
-            <Switch>
+            <Routes>
               {/* <Route path="/signup" component={Signup}/> */}
-              <Route path="/signup" component={<Signup/>}/>
-            </Switch>
+              <Route exact path="/" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+              <Route exact path="/reset" element={<Reset />} />
+              <Route exact path="/dashboard" element={<Dashboard />} />
+            </Routes>
           </AuthProvider>
         </Router>
-        <Signup />
       </div>
     </Container>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from 'firebase/app'
+/*import { initializeApp } from 'firebase/app'
 import { getAuth } from 'firebase/auth'
 
 // console.log('api key : ', process.env.RUMMY_SCORE_TRACKER_FIREBASE_API_KEY)
@@ -22,4 +22,107 @@ const app = initializeApp({
 })
 
 export const auth = getAuth(app)
-export default app
+export default app*/
+
+
+import { initializeApp } from "firebase/app";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+
+import {
+  getFirestore,
+  query,
+  getDocs,
+  collection,
+  where,
+  addDoc,
+} from "firebase/firestore";
+
+const app = initializeApp({
+  apiKey: "AIzaSyDaSmceFuVUP36mJDmdqccjlxSxacjaVg0",
+  authDomain: "rummy-score-tracker.firebaseapp.com",
+  projectId: "rummy-score-tracker",
+  storageBucket: "rummy-score-tracker.appspot.com",
+  messagingSenderId: "420450892849",
+  appId: "1:420450892849:web:acdc3c6f8d9c875d116910"
+});
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const googleProvider = new GoogleAuthProvider();
+const signInWithGoogle = async () => {
+  try {
+    const res = await signInWithPopup(auth, googleProvider);
+    const user = res.user;
+    const q = query(collection(db, "users"), where("uid", "==", user.uid));
+    const docs = await getDocs(q);
+    if (docs.docs.length === 0) {
+      await addDoc(collection(db, "users"), {
+        uid: user.uid,
+        name: user.displayName,
+        authProvider: "google",
+        email: user.email,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "users"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+
+const logout = () => {
+  signOut(auth);
+};
+
+export {
+  auth,
+  db,
+  signInWithGoogle,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
